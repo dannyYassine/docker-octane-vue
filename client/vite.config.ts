3274ea8e95da15a355ab16c0095ffb6b4de4953e
Danny Yassine <dannyyassine@gmail.com>
2022-11-27 02:51:09 +0000

    able to load env vars for vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({command, mode}) => {
   const processEnvViteValues = Object.keys(process.env).reduce((accum: Record<string, any>, key: string) => {
     if (key.startsWith('VITE_APP')) {
       accum[key] = `'${process.env[key]}'`;
