53c4277477b495b75001cf6f5c277c3d8c51828c
“Danny <dannyyassine@gmail.com>
2023-09-22 01:41:27 +0000

    more good stuff

--- api/routes/api.php
+++ api/routes/api.php
@@ -34,7 +34,7 @@ Route::get('/weather/{city}', function (Request $request) {
         $city = $request->route()->parameter('city');
         $appId = Env::get('WEATHER_API_KEY');
 
-        $weatherData = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$appId");
+        $weatherData = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=$city&appid=ba006f6d23994bf75f8af2cc7602ce24");
 
         return json_decode($weatherData);
     };
